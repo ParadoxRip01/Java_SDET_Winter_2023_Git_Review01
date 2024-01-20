5ea8bfce49a315be7a8bfc42ddd04910a34f0877
Iunus <yunus_magdiev01@mail.ru>
2024-01-20 17:03:48 -0500

    Hello World

--- src/package_01/HelloWorld01.java
+++ src/package_01/HelloWorld01.java
@@ -3,6 +3,8 @@ package package_01;
 public class HelloWorld01 {
     public static void main(String[] args) {
         System.out.println("This is Hello  World Class");
+
+        System.out.println("Hello  Cydeo ! ");
     }
 
 }
